299483e4af4114a89762885b312538e2759d79a6
Santiago Pinzon <santiagpinzon@gmail.com>
2024-10-16 15:41:49 -0500

    369

--- src/clase.java
+++ src/clase.java
@@ -1,2 +1,39 @@
-public class clase {
+
+abstract class Vehiculo {
+    protected String marca;
+
+    public Vehiculo(String marca) {
+        this.marca = marca;
+    }
+
+
+    public void arrancar() {
+        System.out.println(marca + " está arrancando...");
+    }
+
+
+    abstract void tipoVehiculo();
+}
+
+
+interface Operable {
+    void mover();
+}
+
+
+public class clase extends Vehiculo implements Operable {
+
+    public clase(String marca) {
+        super(marca);
+    }
+
+
+    void tipoVehiculo() {
+        System.out.println("Este es un coche.");
+    }
+
+
+    public void mover() {
+        System.out.println(marca + " está en movimiento.");
+    }
 }
